182805cfa43adbeacb3f061534c0c38d0bbb535e
setu <setulalchowhan@gmail.com>
2024-09-10 01:02:33 +0600

    initialize all page and complete setup and completed header basic header section and rounting

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element ={<Home/>} />
+      <Route path="/about" element ={<About/>} />
+      <Route path="/sing-in" element ={<Signin/>} />
+      <Route path="/sing-up" element ={<Signup/>} />
+      <Route path="/dashboard" element ={<Dashboard/>} />
+      <Route path="/projects" element ={<Projects/>} />
+    </Routes>
+    
+    </BrowserRouter>
+  
+  )
+}
+
+export default App
