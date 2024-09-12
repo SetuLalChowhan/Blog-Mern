1d4d6383ef016966a9daa42a4092ff8a8cdc46b6
setu <setulalchowhan@gmail.com>
2024-09-12 18:29:49 +0600

    dark mod and tab fuucntionality add

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {  useLocation } from 'react-router-dom'
 
 export default function Dashboard() {
+  const location =useLocation()
+  const [tab,setTab] =useState('')
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl =urlParams.get("tab")
+
+  },[location.search])
   return (
-    <div>Dashboard</div>
+    <div>
+      {/* Sidebar */}
+      <div className=''>
+
+      </div>
+    </div>
   )
 }
