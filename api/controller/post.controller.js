c84328e14897dd9af30ec2e537a25c44fb56b68e
setu <setulalchowhan@gmail.com>
2024-09-17 13:42:44 +0600

    add post section to the dashboard

--- api/controller/post.controller.js
+++ api/controller/post.controller.js
@@ -10,10 +10,10 @@ export const createPost = async (req, res, next) => {
   }
 
   const slug = req.body.title
-    .split(" ")
-    .join("-")
-    .toLowerCase()
-    .replace(/[^a-zA-Z0-9-]/g, "-");
+  .split(' ')
+  .join('-')
+  .toLowerCase()
+  .replace(/[^a-zA-Z0-9-]/g, '');;
   const newPost = new Post({
     ...req.body,
     slug,
