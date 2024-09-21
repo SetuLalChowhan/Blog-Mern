0fbaeb71abe46f7a1f3439451fed572e335ec601
setu <setulalchowhan@gmail.com>
2024-09-21 12:33:57 +0600

    complete post page functionalit

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,37 +1,37 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 
 export const test = (req, res) => {
   res.json({ message: "Api is working" });
 };
 export const updateUser = async (req, res, next) => {
-// 
-if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to update this user'));
+  //
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
   }
   if (req.body.password) {
     if (req.body.password.length < 6) {
-      return next(errorHandler(400, 'Password must be at least 6 characters'));
+      return next(errorHandler(400, "Password must be at least 6 characters"));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
   if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
-        errorHandler(400, 'Username must be between 7 and 20 characters')
+        errorHandler(400, "Username must be between 7 and 20 characters")
       );
     }
-    if (req.body.username.includes(' ')) {
-      return next(errorHandler(400, 'Username cannot contain spaces'));
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
-        console.log(req.body.username)
-      return next(errorHandler(400, 'Username must be lowercase'));
+      console.log(req.body.username);
+      return next(errorHandler(400, "Username must be lowercase"));
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
-        errorHandler(400, 'Username can only contain letters and numbers')
+        errorHandler(400, "Username can only contain letters and numbers")
       );
     }
   }
@@ -50,49 +50,40 @@ if (req.user.id !== req.params.userId) {
       { new: true }
     );
 
-    const {password: pass, ...rest} =updateUser._doc;
-    res.status(200).json(rest)
+    const { password: pass, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
   } catch (err) {
     next(err);
   }
 };
-export const deleteUser = async(req,res,next)=>{
-  if(req.user.id !==req.params.userId){
-    console.log(req.user.id)
-    console.log(req.params.userId)
-    return next(errorHandler(403, "You are not allowed to delete this user"))
+export const deleteUser = async (req, res, next) => {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
   }
-
-  try{
+  try {
     await User.findByIdAndDelete(req.params.userId);
-    res.status(200).json("User has been deleted")
-
-  }catch(error){
-    next(error)
-    console.log(error)
-
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
   }
+};
 
-}
-
-export const signout =(req,res,next)=>{
-  try{
-    res.clearCookie('access_token').status(200).json("User has been sign out")
-
-  }catch(error){
-    console.log(error)
-    next(error)
+export const signout = (req, res, next) => {
+  try {
+    res.clearCookie("access_token").status(200).json("User has been sign out");
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
-
-}
+};
 export const getUsers = async (req, res, next) => {
   if (!req.user.isAdmin) {
-    return next(errorHandler(403, 'You are not allowed to see all users'));
+    return next(errorHandler(403, "You are not allowed to see all users"));
   }
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
 
     const users = await User.find()
       .sort({ createdAt: sortDirection })
@@ -131,7 +122,7 @@ export const getUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.userId);
     if (!user) {
-      return next(errorHandler(404, 'User not found'));
+      return next(errorHandler(404, "User not found"));
     }
     const { password, ...rest } = user._doc;
     res.status(200).json(rest);
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,78 +1,70 @@
-import React, { useRef, useState } from "react";
-import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import { app } from "../firebase.js";
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import {
   getDownloadURL,
   getStorage,
   ref,
   uploadBytesResumable,
-} from "firebase/storage";
-import { CircularProgressbar } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
-import { useNavigate } from "react-router-dom";
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreatePost() {
-  const quillRef = useRef(null);
-  const navigate =useNavigate()
-  const [publisError, setPublishError] = useState(null);
   const [file, setFile] = useState(null);
-  const [imageFileUrl, setImageFileUrl] = useState(null);
-  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
-  const [imageFileUploadError, setImageFileUploadError] = useState(null);
-  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
 
-  const handleUploadImage = async () => {
+  const navigate = useNavigate();
+
+  const handleUpdloadImage = async () => {
     try {
       if (!file) {
-        setImageFileUploadError("Plese select an image");
+        setImageUploadError('Please select an image');
         return;
       }
-      setImageFileUploadError(null);
+      setImageUploadError(null);
       const storage = getStorage(app);
-      const fileName = new Date().getTime() + "-" + file.name;
+      const fileName = new Date().getTime() + '-' + file.name;
       const storageRef = ref(storage, fileName);
       const uploadTask = uploadBytesResumable(storageRef, file);
       uploadTask.on(
-        "state_changed",
+        'state_changed',
         (snapshot) => {
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          setImageFileUploadProgress(progress.toFixed(0));
+          setImageUploadProgress(progress.toFixed(0));
         },
         (error) => {
-          setImageFileUploadError(
-            "Could not Upload image(File must be less than 2MB)"
-          );
-          setImageFileUploadProgress(null);
-          setImageFile(null);
-          setImageFileUrl(null);
-          setImageFileUploading(false);
+          setImageUploadError('Image upload failed');
+          setImageUploadProgress(null);
         },
         () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
-            setImageFileUrl(downloadUrl);
-            setFormData({ ...formData, image: downloadUrl });
-            setImageFileUploading(false);
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData({ ...formData, image: downloadURL });
           });
         }
       );
     } catch (error) {
-      setImageFileUploadError("Image Upload Failed");
-      setImageFileUploadProgress(null);
+      setImageUploadError('Image upload failed');
+      setImageUploadProgress(null);
       console.log(error);
     }
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("/api/post/create", {
-        method: "POST",
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
@@ -81,26 +73,26 @@ export default function CreatePost() {
         setPublishError(data.message);
         return;
       }
-   
+
       if (res.ok) {
         setPublishError(null);
-        navigate(`/post/${data.post.slug}`)
+        navigate(`/post/${data.slug}`);
       }
     } catch (error) {
-      setPublishError("Something went Wrong")
+      setPublishError('Something went wrong');
     }
   };
   return (
-    <div className=" p-3 max-w-7xl mx-auto min-h-screen 0">
-      <h1 className="text-center text-3xl my-7 font-semibold">Create a Post</h1>
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+      <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
-            type="text"
-            placeholder="Tilte"
+            type='text'
+            placeholder='Title'
             required
-            id="title"
-            className="flex-1"
+            id='title'
+            className='flex-1'
             onChange={(e) =>
               setFormData({ ...formData, title: e.target.value })
             }
@@ -110,65 +102,63 @@ export default function CreatePost() {
               setFormData({ ...formData, category: e.target.value })
             }
           >
-            <option value="uncategorized">Select a category</option>
-            <option value="javascript">JavaScript</option>
-            <option value="reactjs">React.js</option>
-            <option value="nextjs">Next.js</option>
+            <option value='uncategorized'>Select a category</option>
+            <option value='javascript'>JavaScript</option>
+            <option value='reactjs'>React.js</option>
+            <option value='nextjs'>Next.js</option>
           </Select>
         </div>
-        <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
+        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
           <FileInput
-            type="file"
-            accept="image/*"
+            type='file'
+            accept='image/*'
             onChange={(e) => setFile(e.target.files[0])}
           />
           <Button
-            type="button"
-            gradientDuoTone="purpleToBlue"
-            size="sm"
+            type='button'
+            gradientDuoTone='purpleToBlue'
+            size='sm'
             outline
-            onClick={handleUploadImage}
-            disabled={imageFileUploadProgress}
+            onClick={handleUpdloadImage}
+            disabled={imageUploadProgress}
           >
-            {imageFileUploadProgress ? (
-              <div className="w-16 h-16">
+            {imageUploadProgress ? (
+              <div className='w-16 h-16'>
                 <CircularProgressbar
-                  value={imageFileUploadProgress}
-                  text={`${imageFileUploadProgress || 0}`}
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
                 />
-                <span>Uploading...</span>
               </div>
             ) : (
-              "Upload Image"
+              'Upload Image'
             )}
           </Button>
         </div>
-        {imageFileUploadError && (
-          <Alert color="failure">{imageFileUploadError}</Alert>
-        )}
+        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
         {formData.image && (
           <img
             src={formData.image}
-            alt="upload"
-            className="w-full h-72 object-cover"
+            alt='upload'
+            className='w-full h-72 object-cover'
           />
         )}
         <ReactQuill
-          ref={quillRef}
-          theme="snow"
-          placeholder="Write something....."
-          className="h-72 mb-12 "
+          theme='snow'
+          placeholder='Write something...'
+          className='h-72 mb-12'
           required
           onChange={(value) => {
             setFormData({ ...formData, content: value });
           }}
         />
-        <Button type="submit" gradientDuoTone={"purpleToPink"}>
+        <Button type='submit' gradientDuoTone='purpleToPink'>
           Publish
         </Button>
-        {
-          publisError  &&<Alert className="mt-5" color={"failure"}>{publisError}</Alert>
-        }
+        {publishError && (
+          <Alert className='mt-5' color='failure'>
+            {publishError}
+          </Alert>
+        )}
       </form>
     </div>
   );
